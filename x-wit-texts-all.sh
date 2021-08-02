4d05450afc15005de160b85e682d0124aa0575e6
enjotel <nilsliersch@web.de>
2021-08-02 11:20:17 +0200

    updated all

--- x-wit-texts-all.sh
+++ x-wit-texts-all.sh
@@ -5,9 +5,9 @@ set -o pipefail
 set -o nounset
 #set -o xtrace # uncomment for debugging
 
-grep -o "wit=\"[^\"]*\"" hp_1.1-20.xml | sed -e "s_\([\"#]\|wit=\|ceteri\)__g" -e "s_ _\n_g" | sed "/^ *$/d" | sort | uniq | \
+grep -o "wit=\"[^\"]*\"" hp_1.1-20-tei.xml | sed -e "s_\([\"#]\|wit=\|ceteri\)__g" -e "s_ _\n_g" | sed "/^ *$/d" | sort | uniq | \
     while read w;
     do
 	echo "Extracting witness ${w}"
-	xsltproc --output wit_texts/hp_1.1-20_"${w}".xml --stringparam wit_id "${w}" x-wit-text.xsl hp_1.1-20.xml
+	xsltproc --output wit_texts/hp_1.1-20_"${w}".xml --stringparam wit_id "${w}" x-wit-text.xsl hp_1.1-20-tei.xml
     done
